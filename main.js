1e51d0365fe153bad23f8f1e9f803480ee2bbf10
David <david.singer@netcentric.biz>
2022-05-16 14:38:40 +0200

    clean up js

--- main.js
+++ main.js
@@ -5,12 +5,3 @@ console.log(article);
 console.log(article.dataset.columns);
 console.log(article.dataset.indexNumber);
 console.log(article.dataset.parent);
-
-const table = document.getElementsByClassName('sorting--table');
-console.log(table);
-
-for (let t of table) {
-  console.log(t);
-  const tBody = table[1];
-  console.log(tBody);
-}
